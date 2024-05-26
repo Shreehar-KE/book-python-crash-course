2b1c19d107bbc74bdfbc5349cf5b1d316f13bcdc
Shreehar-KE <shreehar.elancheran@gmail.com>
2024-05-26 23:15:05 +0530

    revise Chapter-2

--- Exercises/Ch2/name_cases.py
+++ Exercises/Ch2/name_cases.py
@@ -1,3 +1,8 @@
+"""
+2-4. Name Cases: Use a variable to represent a person's name, and then print
+that person's name in lowercase, uppercase, and title case.
+"""
+
 name_1 = "shreehar"
 name_2 = "IVY"
 
