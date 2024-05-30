c2e05424992e3a9b56547d4ec61a5dd82d712191
Shreehar-KE <shreehar.elancheran@gmail.com>
2024-05-30 15:10:25 +0530

    revise Chapter-5

--- Exercises/Ch5/no_users.py
+++ Exercises/Ch5/no_users.py
@@ -1,3 +1,14 @@
+"""
+5-9. No Users: Add an if test to hello_admin.py to make sure the list of users is
+not empty.
+
+• If the list is empty, print the message We need to find some users!
+
+• Remove all of the usernames from your list, and make sure the correct mes-
+  sage is printed.
+"""
+
+
 usernames = []
 # usernames = ['Indiana Jones', 'John Wick','Bruce Almighty', 'admin', 'Jack Sparrow']
 
